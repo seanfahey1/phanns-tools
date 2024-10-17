629b26c9cf6dd69b738b15986b32e4ca91e04094
Sean <sfahey1745@sdsu.edu>
2024-10-17 15:49:16 -0700

    'OTH deletion saves deleted sequences'

--- OTH-cluster-deletion.py
+++ OTH-cluster-deletion.py
@@ -1,5 +1,7 @@
 import argparse
+import re
 import subprocess
+from datetime import datetime
 from pathlib import Path
 
 from Bio import SeqIO
@@ -48,14 +50,69 @@ def get_args():
 
     if args.output is None:
         args.output = args.target.with_suffix(".filtered")
+    args.now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
 
     return args
+
+
+def cd_hit(input_file, output_file, **kwargs):
+    cmd = f"""cd-hit
+        -i {input_file}
+        -o {output_file}
+        -c {kwargs.get("c", 0.4)}
+        -n {kwargs.get("n", 2)}
+        -d 0
+        -M 0
+        -T 0
+        -sc 1
+        -sf 1
+    """
+
+    results = subprocess.run(cmd, shell=True, universal_newlines=True, check=True)
+    return results
+
+
+def digest_clusters(cluster_file_path):
+    assert (
+        cluster_file_path.is_file()
+    ), f"File not found: {cluster_file_path}. CD-hit may have not run properly"
+
+    with open(cluster_file_path) as f:
+        clusters = f.read().split(">Cluster")[1:]
+        clusters = [x for x in clusters if x.strip() != ""]
+
+    for cluster in clusters:
+        # clean up cluster
+        cluster_lines = [x for x in cluster.split("\n").strip() if x != ""]
+
+        # pull just descriptions
+        descriptions = [
+            str(re.search("\d+aa, >(.*@@@-?\d*)\.\.\.", x).group(1))
+            for x in cluster_lines
+        ]
+
+        # skip non-mixed clusters
+        source_files = [x.split("@@@")[0] for x in descriptions]
+        if len(set(source_files)) == 1:
+            continue
+
+        for line in descriptions:
+            source_file, seq_hash = line.split("@@@")
+            yield source_file, seq_hash
+
+
+def write_dict_to_fasta(d, output_file):
+    with open(output_file, "w") as f:
+        for description, seq in d.values():
+            seq_80 = re.sub("(.{64})", "\\1\n", seq, 0, re.DOTALL)
+            f.write(f">{description}\n{seq_80}\n")
 
 
 def main():
     args = get_args()
 
     target_hash_lookup = {}
+    removed_records = {}
     all_records = []
 
     # combine files with hashed headers
@@ -75,25 +132,27 @@ def main():
         all_records.append(record)
 
     # write combined file
-    with open("combined.fasta", "w") as f:
+    with open(f"{args.now}combined.fasta", "w") as f:
         SeqIO.write(all_records, f, "fasta")
 
     # cluster sequences
-    cmd = """cd-hit
-        -i combined.fasta
-        -o combined.fasta.clstr
-        -c 0.4
-        -n 2
-        -d 0
-        -M 0
-        -T 0
-        -sc 1
-        -sf 1
-    """
+    results = cd_hit(f"{args.now}combined.fasta", f"{args.now}combined_out.fasta")
 
+    for source_file, seq_hash in digest_clusters(
+        Path(f"{args.now}combined_out.fasta.clstr")
+    ):
+        if source_file == args.target.stem:
+            if seq_hash not in target_hash_lookup:
+                raise ValueError(
+                    f"Hash from {source_file} not found in target lookup: {seq_hash}"
+                )
 
-    results = subprocess.run(cmd, shell=True, universal_newlines=True, check=True)
-    print(results.stdout)
+            removed_records[seq_hash] = target_hash_lookup.pop(seq_hash)
+
+    write_dict_to_fasta(target_hash_lookup, args.output)
+    write_dict_to_fasta(
+        removed_records, f"{args.output.with_name(args.output.stem + '_removed.fasta')}"
+    )
 
 
 if __name__ == "__main__":
